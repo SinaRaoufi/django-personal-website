3cf29116cf461e0aef094606b1088b3f2c56b4ff
Sina Raoufi <sinaraoufi@outlook.com>
2021-01-02 17:18:23 +0330

    add Comment model

--- blog/models.py
+++ blog/models.py
@@ -37,3 +37,20 @@ class Post(models.Model):
                                                  self.publish.month,
                                                  self.publish.day,
                                                  self.slug])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}"
