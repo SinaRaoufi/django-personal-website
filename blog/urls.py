790821dcef422420d113bec526080038da40ea55
Sina Raoufi <sinaraoufi@outlook.com>
2021-01-05 19:49:54 +0330

    change url of post search

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import PostListView, post_detail
+from .views import PostListView, post_detail, SearchPostList
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('tag/<slug:tag_slug>/', PostListView.as_view(), name='post_list_by_tag'),
+    path('posts/search/', SearchPostList.as_view(), name='search_post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          post_detail, name='post_detail'),
 ]
